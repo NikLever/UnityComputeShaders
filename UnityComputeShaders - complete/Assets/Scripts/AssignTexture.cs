67b151d0f68e4de13a01d850bdc203da90438a2e
Nicholas Lever <nik@catalystpics.co.uk>
2020-10-13 12:58:18 +0100

    Initial commit

---
+++ UnityComputeShaders - complete/Assets/Scripts/AssignTexture.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssignTexture : MonoBehaviour
+{
+
+    public ComputeShader shader;
+    public int texResolution = 256;
+
+    Renderer rend;
+    RenderTexture outputTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+        outputTexture = new RenderTexture(texResolution, texResolution, 24);
+        outputTexture.enableRandomWrite = true;
+        outputTexture.Create();
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+
+        UpdateTextureFromCompute();
+    }
+
+    private void UpdateTextureFromCompute()
+    {
+        int kernelHandle = shader.FindKernel("CSMain");
+        
+        shader.SetTexture(kernelHandle, "Result", outputTexture);
+        shader.Dispatch(kernelHandle, texResolution / 8, texResolution / 8, 1);
+
+        rend.material.SetTexture("_MainTex", outputTexture);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.U))
+            UpdateTextureFromCompute();
+    }
+}
+
---
+++ UnityComputeShaders - complete/Assets/Scripts/SolidColor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SolidColor : MonoBehaviour
+{
+
+    public ComputeShader shader;
+    public int texResolution = 256;
+    public string kernelName = "SolidRed";
+
+    Renderer rend;
+    RenderTexture outputTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+        outputTexture = new RenderTexture(texResolution, texResolution, 24);
+        outputTexture.enableRandomWrite = true;
+        outputTexture.Create();
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+
+        UpdateTextureFromCompute();
+    }
+
+    private void UpdateTextureFromCompute()
+    {
+        int kernelHandle = shader.FindKernel(kernelName);
+        
+        shader.SetTexture(kernelHandle, "Result", outputTexture);
+        shader.Dispatch(kernelHandle, texResolution / 8, texResolution / 8, 1);
+
+        rend.material.SetTexture("_MainTex", outputTexture);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.U))
+            UpdateTextureFromCompute();
+    }
+}
+
