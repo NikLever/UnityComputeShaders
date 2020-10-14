8d28bc5f416f36cdde4ebef245ed700ef66ecd92
Nicholas Lever <nik.j.lever@gmail.com>
2020-10-14 14:12:58 +0100

    Updating lecture 1.3

--- UnityComputeShaders - complete/Assets/Scripts/AssignTexture.cs
+++ UnityComputeShaders - complete/Assets/Scripts/AssignTexture.cs
@@ -10,33 +10,44 @@ public class AssignTexture : MonoBehaviour
     Renderer rend;
     RenderTexture outputTexture;
 
+    int kernelHandle;
+
     // Use this for initialization
     void Start()
     {
-        outputTexture = new RenderTexture(texResolution, texResolution, 24);
+        outputTexture = new RenderTexture(texResolution, texResolution, 0);
         outputTexture.enableRandomWrite = true;
         outputTexture.Create();
 
         rend = GetComponent<Renderer>();
         rend.enabled = true;
 
-        UpdateTextureFromCompute();
+        InitShader();
     }
 
-    private void UpdateTextureFromCompute()
+    private void InitShader()
     {
-        int kernelHandle = shader.FindKernel("CSMain");
-        
+        kernelHandle = shader.FindKernel("CSMain");
+
         shader.SetTexture(kernelHandle, "Result", outputTexture);
-        shader.Dispatch(kernelHandle, texResolution / 8, texResolution / 8, 1);
 
         rend.material.SetTexture("_MainTex", outputTexture);
+
+        DispatchShader(texResolution / 16, texResolution / 16);
+    }
+
+    private void DispatchShader(int x, int y)
+    {
+        shader.Dispatch(kernelHandle, x, y, 1);
     }
 
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.U))
-            UpdateTextureFromCompute();
+        {
+            Debug.Log("DispatchShader");
+            DispatchShader(texResolution / 8, texResolution / 8);
+        }
     }
 }
 
