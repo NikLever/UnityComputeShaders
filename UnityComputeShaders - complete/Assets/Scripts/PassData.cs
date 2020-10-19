05689cf8f7c6cdecd868fb4175fdc57a1d9e3172
Nicholas Lever <nik.j.lever@gmail.com>
2020-10-19 14:25:17 +0100

    Challenge 1

---
+++ UnityComputeShaders - complete/Assets/Scripts/PassData.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassData : MonoBehaviour
+{
+
+    public ComputeShader shader;
+    public int texResolution = 1024;
+
+    Renderer rend;
+    RenderTexture outputTexture;
+
+    int clearHandle;
+    int circlesHandle;
+
+    public Color clearColor = new Color();
+    public Color circleColor = new Color();
+
+    // Use this for initialization
+    void Start()
+    {
+        outputTexture = new RenderTexture(texResolution, texResolution, 0);
+        outputTexture.enableRandomWrite = true;
+        outputTexture.Create();
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+
+        InitShader();
+    }
+
+    private void InitShader()
+    {
+        clearHandle = shader.FindKernel("Clear");
+        circlesHandle = shader.FindKernel("Circles");
+
+        shader.SetVector( "clearColor", clearColor );
+        shader.SetVector( "circleColor", circleColor );
+        shader.SetTexture( clearHandle, "Result", outputTexture);
+        shader.SetTexture( circlesHandle, "Result", outputTexture);
+
+        rend.material.SetTexture("_MainTex", outputTexture);
+
+        DispatchKernels(10);
+    }
+ 
+    private void DispatchKernels(int count)
+    {
+        //shader.Dispatch(clearHandle, texResolution / 8, texResolution / 8, 1);
+        shader.Dispatch(circlesHandle, count, 1, 1);
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
---
+++ UnityComputeShaders - start/Assets/Scripts/Challenge1.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Challenge1 : MonoBehaviour
+{
+
+    public ComputeShader shader;
+    public int texResolution = 1024;
+
+    Renderer rend;
+    RenderTexture outputTexture;
+
+    int kernelHandle;
+
+    // Use this for initialization
+    void Start()
+    {
+        outputTexture = new RenderTexture(texResolution, texResolution, 0);
+        outputTexture.enableRandomWrite = true;
+        outputTexture.Create();
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+
+        InitShader();
+    }
+
+    private void InitShader()
+    {
+        kernelHandle = shader.FindKernel("Square");
+
+		//Create a Vector4 with parameters x, y, width, height
+        //Pass this to the shader using SetVector
+        
+        shader.SetTexture(kernelHandle, "Result", outputTexture);
+       
+        rend.material.SetTexture("_MainTex", outputTexture);
+
+        DispatchShader(texResolution / 8, texResolution / 8);
+    }
+
+    private void DispatchShader(int x, int y)
+    {
+        shader.Dispatch(kernelHandle, x, y, 1);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.U))
+        {
+            DispatchShader(texResolution / 8, texResolution / 8);
+        }
+    }
+}
+
