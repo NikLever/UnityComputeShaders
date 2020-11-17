3f66f6aeb4884788063245174ae335ea6d8ed2b0
Nicholas Lever <nik.j.lever@gmail.com>
2020-11-17 16:30:11 +0000

    Particle effects

---
+++ UnityComputeShaders - complete/Assets/Scripts/ParticleFun.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFun : MonoBehaviour
+{
+
+    private Vector2 cursorPos;
+
+    // struct
+    struct Particle
+    {
+        public Vector3 position;
+        public Vector3 velocity;
+        public float life;
+    }
+
+    const int SIZE_PARTICLE = 7 * sizeof(float);
+
+    public int particleCount = 1000000;
+    public Material material;
+    public ComputeShader shader;
+    [Range(1, 10)]
+    public int pointSize = 2;
+
+    int kernelID;
+    ComputeBuffer particleBuffer;
+
+    int groupSizeX; 
+    
+    
+    // Use this for initialization
+    void Start()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        // initialize the particles
+        Particle[] particleArray = new Particle[particleCount];
+
+        for (int i = 0; i < particleCount; i++)
+        {
+            float x = Random.value * 2 - 1.0f;
+            float y = Random.value * 2 - 1.0f;
+            float z = Random.value * 2 - 1.0f;
+            Vector3 xyz = new Vector3(x, y, z);
+            xyz.Normalize();
+            xyz *= Random.value;
+            xyz *= 0.5f;
+
+
+            particleArray[i].position.x = xyz.x;
+            particleArray[i].position.y = xyz.y;
+            particleArray[i].position.z = xyz.z + 3;
+
+            particleArray[i].velocity.x = 0;
+            particleArray[i].velocity.y = 0;
+            particleArray[i].velocity.z = 0;
+
+            // Initial life value
+            particleArray[i].life = Random.value * 5.0f + 1.0f;
+        }
+
+        // create compute buffer
+        particleBuffer = new ComputeBuffer(particleCount, SIZE_PARTICLE);
+
+        particleBuffer.SetData(particleArray);
+
+        // find the id of the kernel
+        kernelID = shader.FindKernel("CSParticle");
+
+        uint threadsX;
+        shader.GetKernelThreadGroupSizes(kernelID, out threadsX, out _, out _);
+        groupSizeX = Mathf.CeilToInt((float)particleCount / (float)threadsX);
+
+        // bind the compute buffer to the shader and the compute shader
+        shader.SetBuffer(kernelID, "particleBuffer", particleBuffer);
+        material.SetBuffer("particleBuffer", particleBuffer);
+
+        material.SetInt("_PointSize", pointSize);
+    }
+
+    void OnRenderObject()
+    {
+        material.SetPass(0);
+        Graphics.DrawProceduralNow(MeshTopology.Points, 1, particleCount);
+    }
+
+    void OnDestroy()
+    {
+        if (particleBuffer != null)
+            particleBuffer.Release();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float[] mousePosition2D = { cursorPos.x, cursorPos.y };
+
+        // Send datas to the compute shader
+        shader.SetFloat("deltaTime", Time.deltaTime);
+        shader.SetFloats("mousePosition", mousePosition2D);
+
+        // Update the Particles
+        shader.Dispatch(kernelID, groupSizeX, 1, 1);
+    }
+
+    void OnGUI()
+    {
+        Vector3 p = new Vector3();
+        Camera c = Camera.main;
+        Event e = Event.current;
+        Vector2 mousePos = new Vector2();
+
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        mousePos.x = e.mousePosition.x;
+        mousePos.y = c.pixelHeight - e.mousePosition.y;
+
+        p = c.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, c.nearClipPlane + 14));// z = 3.
+
+        cursorPos.x = p.x;
+        cursorPos.y = p.y;
+        
+    }
+}
---
+++ UnityComputeShaders - complete/Assets/Shaders/Particle.shader
@@ -0,0 +1,67 @@
+﻿Shader "Custom/Particle" {
+	Properties     
+    {         
+        _PointSize("Point size", Float) = 5.0     
+    }  
+
+	SubShader {
+		Pass {
+		Tags{ "RenderType" = "Opaque" }
+		LOD 200
+		Blend SrcAlpha one
+
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma vertex vert
+		#pragma fragment frag
+
+		uniform float _PointSize;
+
+		#include "UnityCG.cginc"
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 5.0
+
+		struct Particle{
+			float3 position;
+			float3 velocity;
+			float life;
+		};
+		
+		struct PS_INPUT{
+			float4 position : SV_POSITION;
+			float4 color : COLOR;
+			float life : LIFE;
+			float size: PSIZE;
+		};
+		// particles' data
+		StructuredBuffer<Particle> particleBuffer;
+		
+
+		PS_INPUT vert(uint vertex_id : SV_VertexID, uint instance_id : SV_InstanceID)
+		{
+			PS_INPUT o = (PS_INPUT)0;
+
+			// Color
+			float life = particleBuffer[instance_id].life;
+			float lerpVal = life * 0.25f;
+			o.color = fixed4(1.0f - lerpVal+0.1, lerpVal+0.1, 1.0f, lerpVal);
+
+			// Position
+			o.position = UnityObjectToClipPos(float4(particleBuffer[instance_id].position, 1.0f));
+			o.size = _PointSize;
+
+			return o;
+		}
+
+		float4 frag(PS_INPUT i) : COLOR
+		{
+			return i.color;
+		}
+
+
+		ENDCG
+		}
+	}
+	FallBack Off
+}
