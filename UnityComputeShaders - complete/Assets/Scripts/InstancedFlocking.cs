af07dcdb18c03bacee3ac43ce3a7897c17fa738f
Nicholas Lever <nik.j.lever@gmail.com>
2020-11-21 12:28:36 +0000

    Simple and Instanced Flocking

---
+++ UnityComputeShaders - complete/Assets/Scripts/InstancedFlocking.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstancedFlocking : MonoBehaviour
+{
+    public struct Boid
+    {
+        public Vector3 position;
+        public Vector3 direction;
+        public float noise_offset;
+
+        public Boid(Vector3 pos, Vector3 dir, float offset)
+        {
+            position.x = pos.x;
+            position.y = pos.y;
+            position.z = pos.z;
+            direction.x = dir.x;
+            direction.y = dir.y;
+            direction.z = dir.z;
+            noise_offset = offset;
+        }
+    }
+
+    public ComputeShader shader;
+
+    public float rotationSpeed = 1f;
+    public float boidSpeed = 1f;
+    public float neighbourDistance = 1f;
+    public float boidSpeedVariation = 1f;
+    public Mesh boidMesh;
+    public Material boidMaterial;
+    public int boidsCount;
+    public float spawnRadius;
+    public Transform target;
+
+    int kernelHandle;
+    ComputeBuffer boidsBuffer;
+    ComputeBuffer argsBuffer;
+    uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+    Boid[] boidsArray;
+    GameObject[] boids;
+    int groupSizeX;
+    int numOfBoids;
+    Bounds bounds;
+
+    void Start()
+    {
+        kernelHandle = shader.FindKernel("CSMain");
+
+        uint x;
+        shader.GetKernelThreadGroupSizes(kernelHandle, out x, out _, out _);
+        groupSizeX = Mathf.CeilToInt((float)boidsCount / (float)x);
+        numOfBoids = groupSizeX * (int)x;
+
+        bounds = new Bounds(Vector3.zero, Vector3.one * 1000);
+
+        InitBoids();
+        InitShader();
+    }
+
+    private void InitBoids()
+    {
+        boids = new GameObject[numOfBoids];
+        boidsArray = new Boid[numOfBoids];
+
+        for (int i = 0; i < numOfBoids; i++)
+        {
+            Vector3 pos = transform.position + Random.insideUnitSphere * spawnRadius;
+            Quaternion rot = Quaternion.Slerp(transform.rotation, Random.rotation, 0.3f);
+            float offset = Random.value * 1000.0f;
+            boidsArray[i] = new Boid(pos, rot.eulerAngles, offset);
+        }
+    }
+
+    void InitShader()
+    {
+        boidsBuffer = new ComputeBuffer(numOfBoids, 7 * sizeof(float));
+        boidsBuffer.SetData(boidsArray);
+
+        argsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
+        if (boidMesh != null)
+        {
+            args[0] = (uint)boidMesh.GetIndexCount(0);
+            args[1] = (uint)numOfBoids;
+            args[2] = (uint)boidMesh.GetIndexStart(0);
+            args[3] = (uint)boidMesh.GetBaseVertex(0);
+        }
+        argsBuffer.SetData(args);
+
+        shader.SetBuffer(this.kernelHandle, "boidsBuffer", boidsBuffer);
+        shader.SetFloat("rotationSpeed", rotationSpeed);
+        shader.SetFloat("boidSpeed", boidSpeed);
+        shader.SetFloat("boidSpeedVariation", boidSpeedVariation);
+        shader.SetVector("flockPosition", target.transform.position);
+        shader.SetFloat("neighbourDistance", neighbourDistance);
+        shader.SetInt("boidsCount", numOfBoids);
+
+        boidMaterial.SetBuffer("boidsBuffer", boidsBuffer);
+    }
+
+    void Update()
+    {
+        shader.SetFloat("time", Time.time);
+        shader.SetFloat("deltaTime", Time.deltaTime);
+
+        shader.Dispatch(this.kernelHandle, groupSizeX, 1, 1);
+
+        Graphics.DrawMeshInstancedIndirect(boidMesh, 0, boidMaterial, bounds, argsBuffer, 0);
+    }
+
+    void OnDestroy()
+    {
+        if (boidsBuffer != null)
+        {
+            boidsBuffer.Dispose();
+        }
+
+        if (argsBuffer != null)
+        {
+            argsBuffer.Dispose();
+        }
+    }
+}
+
