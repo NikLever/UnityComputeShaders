fc22c423302a4457a628ca4d2f7764c872b0d7fa
Nicholas Lever <nik@catalystpics.co.uk>
2020-12-21 14:57:27 +0000

    Screencast 5.6

--- UnityComputeShaders - start/Assets/Scripts/Voxelization.cs
+++ UnityComputeShaders - start/Assets/Scripts/Voxelization.cs
@@ -8,11 +8,6 @@ public class Voxelization : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        VoxelizeMesh voxelizeMesh = GetComponent<VoxelizeMesh>();
-        voxelizeMesh.Voxelize(voxelizeMesh.meshToVoxelize);
-        float pS = voxelizeMesh.ParticleSize;
-        Vector3 scale = new Vector3(pS, pS, pS);
-
-        //Create balls
+        
     }
 }
